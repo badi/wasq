72328875bbe6b882e9e3845b5cffb8ed8af9fd59
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2014-08-22 09:23:07 -0400

    alignment formatting

--- default.nix
+++ default.nix
@@ -23,15 +23,15 @@ let
   };
 
   # depencencies
-  mdtraj = callPackage ./nix/mdtraj.nix (pythonEnv // {inherit cython numpy scipy pandas nose;});
-  cctools = callPackage ./nix/cctools.nix (buildEnv // {inherit which swig libzip zlib perl fuse openssl python;});
-  prody = callPackage ./nix/prody.nix (pythonEnv // {inherit numpy;});
-  pxul = callPackage ./nix/pxul.nix pythonEnv;
-  mdprep = callPackage ./nix/mdprep.nix (pythonEnv // { inherit pyyaml mdtraj prody; });
-  pwq = callPackage ./nix/pwq.nix (pythonEnv // {inherit cctools pyyaml pyzmq;});
-  mdq = callPackage ./nix/mdq.nix (pythonEnv // {inherit cctools mdprep pwq guamps prody mdtraj pyyaml pyzmq;});
-  gromacs = callPackage ./nix/gromacs.nix {inherit stdenv fetchurl cmake; fftw = fftwSinglePrec; };
-  guamps = callPackage ./nix/guamps.nix (buildEnv // {inherit cmake gromacs;});
+  mdtraj    = callPackage ./nix/mdtraj.nix  (pythonEnv      // {inherit cython numpy scipy pandas nose;});
+  cctools   = callPackage ./nix/cctools.nix (buildEnv       // {inherit which swig libzip zlib perl fuse openssl python;});
+  prody     = callPackage ./nix/prody.nix   (pythonEnv      // {inherit numpy;});
+  pxul      = callPackage ./nix/pxul.nix     pythonEnv;
+  mdprep    = callPackage ./nix/mdprep.nix  (pythonEnv      // { inherit pyyaml mdtraj prody; });
+  pwq       = callPackage ./nix/pwq.nix     (pythonEnv      // {inherit cctools pyyaml pyzmq;});
+  mdq       = callPackage ./nix/mdq.nix     (pythonEnv      // {inherit cctools mdprep pwq guamps prody mdtraj pyyaml pyzmq;});
+  gromacs   = callPackage ./nix/gromacs.nix {inherit stdenv fetchurl cmake; fftw = fftwSinglePrec; };
+  guamps    = callPackage ./nix/guamps.nix  (buildEnv       // {inherit cmake gromacs;});
 
 in
 {
