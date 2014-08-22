b5112133a0547a1125339ab029f48fc22da1e38a
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2014-08-22 09:21:07 -0400

    cleanup

--- default.nix
+++ default.nix
@@ -5,46 +5,35 @@ let
   licenses = pkgs.lib.licenses;
   platforms = pkgs.lib.platforms;
 
+in
+
+with pkgs;
+with python27Packages;
+
+let
   pythonEnv = {
-      inherit (pkgs) stdenv buildPythonPackage fetchurl;
-      inherit (pkgs.lib) licenses platforms;
+      inherit stdenv buildPythonPackage fetchurl;
+      inherit licenses platforms;
       inherit python;
     };
 
   buildEnv = {
-    inherit (pkgs) stdenv fetchurl;
-    inherit (pkgs.lib) licenses platforms;
+    inherit stdenv fetchurl;
+    inherit licenses platforms;
   };
 
-  pythonPackages = pkgs.python27Packages;
-  python = pythonPackages.python;
-  pyyaml = pythonPackages.pyyaml;
-  pyzmq = pythonPackages.pyzmq;
-  cython = pythonPackages.cython;
-  numpy = pythonPackages.numpy;
-  scipy = pythonPackages.scipy;
-  pandas = pythonPackages.pandas;
-  nose = pythonPackages.nose;
-
-
-  mdtraj = pkgs.callPackage ./nix/mdtraj.nix (pythonEnv // {inherit cython numpy scipy pandas nose;});
-  cctools = pkgs.callPackage ./nix/cctools.nix (buildEnv // {
-    inherit (pkgs) which swig libzip zlib perl fuse openssl;
-    inherit python;
-  });
-  prody = pkgs.callPackage ./nix/prody.nix (pythonEnv // {inherit numpy;});
-  pxul = pkgs.callPackage ./nix/pxul.nix pythonEnv;
-  mdprep = pkgs.callPackage ./nix/mdprep.nix (pythonEnv // { inherit pyyaml mdtraj prody; });
-  pwq = pkgs.callPackage ./nix/pwq.nix (pythonEnv // {inherit cctools pyyaml pyzmq;});
-  mdq = pkgs.callPackage ./nix/mdq.nix (pythonEnv // {inherit cctools mdprep pwq guamps prody mdtraj pyyaml pyzmq;});
-  gromacs = with pkgs;
-            pkgs.callPackage ./nix/gromacs.nix {inherit stdenv fetchurl cmake;
-	                                        fftw = fftwSinglePrec; };
-  guamps = with pkgs; pkgs.callPackage ./nix/guamps.nix (buildEnv // {inherit cmake gromacs;});
+  # depencencies
+  mdtraj = callPackage ./nix/mdtraj.nix (pythonEnv // {inherit cython numpy scipy pandas nose;});
+  cctools = callPackage ./nix/cctools.nix (buildEnv // {inherit which swig libzip zlib perl fuse openssl python;});
+  prody = callPackage ./nix/prody.nix (pythonEnv // {inherit numpy;});
+  pxul = callPackage ./nix/pxul.nix pythonEnv;
+  mdprep = callPackage ./nix/mdprep.nix (pythonEnv // { inherit pyyaml mdtraj prody; });
+  pwq = callPackage ./nix/pwq.nix (pythonEnv // {inherit cctools pyyaml pyzmq;});
+  mdq = callPackage ./nix/mdq.nix (pythonEnv // {inherit cctools mdprep pwq guamps prody mdtraj pyyaml pyzmq;});
+  gromacs = callPackage ./nix/gromacs.nix {inherit stdenv fetchurl cmake; fftw = fftwSinglePrec; };
+  guamps = callPackage ./nix/guamps.nix (buildEnv // {inherit cmake gromacs;});
 
 in
-with pkgs;
-with python27Packages;
 {
   devEnv = buildPythonPackage rec {
     version = "0.1.0";
